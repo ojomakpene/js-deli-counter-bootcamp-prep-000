eae90298ed68626172388bc54be77a3b319221e9
Sani Abubakar <egbaojo@gmail.com>
2017-08-29 12:22:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var katzDeli = []
 var otherDeli = ["Steven", "Blake", "Avi"];
 function takeANumber() {
-  katzDeli.push("Ada")
+katzDeli.unshift('Ada')
 console.log(katzDeli)
 return 'Welcome, Ada. You are number 1 in line.'
 }
