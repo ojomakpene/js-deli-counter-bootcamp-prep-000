45aa4754efb65fcd4b7a2b79910d5211b4691016
Sani Abubakar <egbaojo@gmail.com>
2017-08-29 12:22:34 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+var katzDeli = []
+var otherDeli = ["Steven", "Blake", "Avi"];
+function takeANumber() {
+  katzDeli.push("Ada")
+console.log(katzDeli)
+return 'Welcome, Ada. You are number 1 in line.'
+}
